bb3130da1d332ce81069336550aae772856395f2
Ashish Prajapati <116935348+Anticoder03@users.noreply.github.com>
2025-12-14 19:12:58 +0530

    update the app

--- app/app.py
+++ app/app.py
@@ -1,13 +1,21 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, jsonify
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
+@app.route("/journey")
+def journey():
+    return render_template("journey.html")
+
 @app.route("/health")
 def health():
+    return jsonify(status="ok")
+
+@app.route("/health-ui")
+def health_ui():
     return render_template("health.html")
 
 if __name__ == "__main__":
